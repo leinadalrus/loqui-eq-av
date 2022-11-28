eea2bd0bb7c95130342a12473ef580fa54d46068
Daniel David Sansano Surla <dandavesan@gmail.com>
2022-11-29 08:14:47 +1100

    Update open_plal_p.cpp

--- src/components/open_plal_p.cpp
+++ src/components/open_plal_p.cpp
@@ -11,6 +11,9 @@ _Pragma("GCC diagnostic ignored \"-Wold-style-cast\"")
 
 #include "open_plal_t.h"
 
+#define GRAPHICS_API_OPENGL_43
+#include <rlgl.h>
+
 #define PL_MPEG_IMPLEMENTATION
 #include <pl_mpeg.h>
 
@@ -40,8 +43,7 @@ const char *source_frag_shader_ycrcb(Texture2D texture_y,
   };
 }
 
-const char * source_shader_rgb(Texture2D texture_rgb;
-                               Vector2 tex_coord) {
+const char * source_shader_rgb(Texture2D texture_rgb, Vector2 tex_coord) {
   return (char*) {
     gl_FragColor = vec4((Texture2D)(texture_rgba, tex_coords).rgb, 1.0);
   };
@@ -66,12 +68,47 @@ OpenPLAL_t *OpenPLAL_t::create_prog_self(const char *filename, int texture_mode)
   plm_set_loop(this->plm, FALSE);
   plm_set_audio_enabled(this->plm, FALSE); // TODO(PL_MPEG2): turn it back on!
   plm_set_audio_stream(this->plm, 0);
+
+  // Windows, Linux
+  glewExperimental = GL_TRUE;
+  glewInit();
+  // Raylib4.2: OpenGL4.3
+  rlglInit(plm_get_width(this->plm), plm_get_height(this->plm));
+
+  const char * fsh = this->texture_mode == source_vertex_shader
+    ? source_frag_shader_ycrcb
+    : source_shader_rgb;
+  
+  this->fragment_shader = app_compile_shader(self, GL_FRAGMENT_SHADER, fsh);
+  this->vertex_shader = app_compile_shader(self, GL_VERTEX_SHADER, APP_VERTEX_SHADER);
+  
+  this->shader_program = glCreateProgram();
+  glAttachShader(this->shader_program, this->vertex_shader);
+  glAttachShader(this->shader_program, this->fragment_shader);
+  glLinkProgram(this->shader_program);
+  glUseProgram(this->shader_program);
+  
+  // Create textures for YCrCb or RGB rendering
+  if (this->texture_mode == APP_TEXTURE_MODE_YCRCB) {
+    this->texture_y  = app_create_texture(self, 0, "texture_y");
+    this->texture_cb = app_create_texture(self, 1, "texture_cb");
+    this->texture_cr = app_create_texture(self, 2, "texture_cr");
+  }
+  else {
+    this->texture_rgb = app_create_texture(self, 0, "texture_rgb");
+    int num_pixels = plm_get_width(this->plm) * plm_get_height(this->plm);
+    this->rgb_data = (uint8_t*)malloc(num_pixels * 3);
+  }
 }
 
-void OpenPLAL_t::update_prog_self(OpenPLAL_t *self) {}
+void OpenPLAL_t::update_prog_self(OpenPLAL_t *self) {
+  rlEnableDepthTest(/*void||self->rlgl_val_m*/);        // Enable depth test
+  rlDisableDepthTest(/*void||self->rlgl_val_m*/);       // Disable depth test
+}
 
 void OpenPLAL_t::destroy_prog_self(OpenPLAL_t *self) {
   plm_destroy(this->plm);
+  rlglClose(/*void*/);
   delete self;
   self = nullptr;
   // free(self);
